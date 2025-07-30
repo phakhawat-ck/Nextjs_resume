b6d968a8f2b9f9a138bade3f702f4ab907c4df0d
Phakhawat <phakhawat.ck@gmail.com>
2025-07-31 03:20:29 +0700

    add gridd project & img profile

--- src/app/components/CardProject.tsx
+++ src/app/components/CardProject.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+interface CardProjectProps {
+  label: string;
+  discription: string;
+}
 
-export default function CardProject() {
+export default function CardProject({ label, discription }: CardProjectProps) {
   return (
-    <div>CardProject</div>
+    <>
+      <div className="border rounded-lg p-4 shadow-sm bg-white ">
+        <h3 className="text-lg font-semibold">{label}</h3>
+        <p className="text-sm text-gray-600 mt-2">{discription}</p>
+        <div className="flex justify-end mt-4">
+          <a href="#" className="text-blue-500 hover:underline text-sm">View Project</a>
+        </div>
+      </div>
+
+    </>
   )
 }
